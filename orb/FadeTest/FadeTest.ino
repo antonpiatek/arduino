730ec6535ff12a00e1a9a414893a4ce08b752bdc
Anton <anton@piatek.co.uk>
2013-03-29 11:17:08 +0000

    tweaked orb fadetest

--- orb/FadeTest/FadeTest.ino
+++ orb/FadeTest/FadeTest.ino
@@ -7,11 +7,11 @@
  This example code is in the public domain.
  */
 
-int ledR = 8;           // the pin that the LED is attached to
-int ledG = 9;           // the pin that the LED is attached to
-int ledB = 10;           // the pin that the LED is attached to
-int brightnessR = 0;    // how bright the LED is
-int brightnessG = 0;    // how bright the LED is
+int ledR = 9;           // the pin that the LED is attached to
+int ledG = 10;           // the pin that the LED is attached to
+int ledB = 11;           // the pin that the LED is attached to
+int brightnessR = 255;    // how bright the LED is
+int brightnessG = 125;    // how bright the LED is
 int brightnessB = 0;    // how bright the LED is
 int fadeAmountR = 5;    // how many points to fade the LED by
 int fadeAmountG = 7;    // how many points to fade the LED by
@@ -19,7 +19,7 @@ int fadeAmountB = 9;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup()  { 
-  // declare pin 9 to be an output:
+  // declare output pins
   pinMode(ledR, OUTPUT);
   pinMode(ledG, OUTPUT);
   pinMode(ledB, OUTPUT);
@@ -47,24 +47,44 @@ void loop()  {
   analogWrite(ledR, brightnessR);    
   analogWrite(ledG, brightnessG);    
   analogWrite(ledB, brightnessB);    
-    
-
-    // change the brightness for next time through the loop:
-    brightnessR = brightnessR + fadeAmountR;
-    brightnessG = brightnessG + fadeAmountG;
-    brightnessB = brightnessB + fadeAmountB;
   
-    // reverse the direction of the fading at the ends of the fade: 
-    if (brightnessR == 0 || brightnessR >= 255) {
-      fadeAmountR = -fadeAmountR ; 
-    }
-    if (brightnessG == 0 || brightnessG >= 255) {
-      fadeAmountG = -fadeAmountG ; 
-    }
-    if (brightnessB == 0 || brightnessB >= 255) {
-      fadeAmountB = -fadeAmountB ; 
-    }    
-    // wait for 30 milliseconds to see the dimming effect    
-    delay(100);
+  // change the brightness for next time through the loop:
+  brightnessR = brightnessR + fadeAmountR;
+  brightnessG = brightnessG + fadeAmountG;
+  brightnessB = brightnessB + fadeAmountB;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightnessR <= 0 || brightnessR >= 255) {
+    fadeAmountR = -fadeAmountR ; 
+  }
+  if (brightnessR < 0) {
+    brightnessR = 0;
+  }
+  else if (brightnessR > 255 ) {
+    brightnessR = 255;
+  }
+  
+  if (brightnessG <= 0 || brightnessG >= 255) {
+    fadeAmountG = -fadeAmountG ; 
+  }
+  if (brightnessG < 0) {
+    brightnessG = 0;
+  }
+  else if (brightnessG > 255 ) {
+    brightnessG = 255;
+  }
+  
+  if (brightnessB <= 0 || brightnessB >= 255) {
+    fadeAmountB = -fadeAmountB; 
+  }    
+  if (brightnessB < 0) {
+    brightnessB = 0;
+  }
+  else if (brightnessB > 255 ) {
+    brightnessB = 255;
+  }
+  
+  // wait for 100 milliseconds to see the dimming effect    
+  delay(100);
 }
 
